7101d08ceb61b3bc1a1c8a83f55d3c05049d61f3
ZAWADBINSHARIF <syedzawadbinsharif@gmail.com>
2026-06-19 22:19:33 +0600

    feat: update contact phone link to use dynamic value from Contact model

--- resources/views/components/fab-button/⚡index.blade.php
+++ resources/views/components/fab-button/⚡index.blade.php
@@ -1,15 +1,21 @@
 <?php
 
+use App\Models\Contact;
 use Livewire\Component;
 
 new class extends Component
 {
-    //
+    public ?Contact $contact = null;
+
+    public function mount(): void
+    {
+        $this->contact = Contact::first();
+    }
 };
 ?>
 
 <span class="fixed md:hidden bottom-20 right-5 z-10">
-    <a href="tel:+8801732754990"
+    <a href="tel:{{$contact->phone?? '09678771880'}}"
         class="w-14 h-14 flex justify-center items-center bg-primary-600 rounded-full shadow-lg hover:bg-primary-700 hover:scale-110 active:scale-95 transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-primary-400 focus:ring-offset-2">
         <x-heroicon-s-phone class="text-white w-6 h-6" />
     </a>
